402b6522465cb8ce73d81f9b8d741f7d4a450d08
Adem <adem668123@gmail.com>
2024-09-24 23:25:56 +0300

    all comit

--- java.js
+++ java.js
@@ -1,5 +1,22 @@
+//Task2
 for (let i = 0; i <= 100; i++) {
     if (i % 3 === 0) {
         alert(i);
     }
 }
+
+//Task3
+function factorial(nuber) {
+    if (number < 0) return "Факториал не определён для отрицательных чисел";
+    if (number === 0 || number === 1) return 1;
+    
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+const number = prompt("Введите число:");
+const fact = factorial(parseInt(number));
+alert(`Факториал числа ${number} равен ${fact}`);
